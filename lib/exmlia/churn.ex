75ab1fe6dbbafc6ed3ad5ad1b763d291e8e78f3b
Keisuke TAKAHASHI <keithseahus@gmail.com>
2014-07-28 23:21:12 +0900

    Add skeleton of churn.ex

---
+++ lib/exmlia/churn.ex
@@ -0,0 +1,21 @@
+defmodule Exmlia.churn do
+
+  def start(port, boot_ip, boot_port, lambda, suffix) do
+  end
+
+  def stop(suffix) do
+  end
+
+  def run(port, boot_ip, boot_port, lambda, suffix) do
+  end
+
+  def exp_rand(lambda) do
+  end
+
+  def join(ip, port, n, pid) do
+  end
+
+  def join(_, _, _, _) do
+  end
+
+end
