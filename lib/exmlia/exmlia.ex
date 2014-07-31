090dafcd932c24d56b60079c9ccfe0c167bb6db3
Keisuke TAKAHASHI <keithseahus@gmail.com>
2014-07-31 22:55:13 +0900

    Update exmlia.ex

--- lib/exmlia/exmlia.ex
+++ lib/exmlia/exmlia.ex
@@ -1,24 +1,43 @@
 defmodule Exmlia.exmlia do
 
   def start(port) do
+    start(port, "")
   end
 
   def start(port, suffix) do
   end
 
   def start0(_port, _suffix, _id, n, _run) do
+    if n > 100 do: true
   end
 
   def start0(port, suffix, id, n, run) do
   end
 
   def is_exist() do
+    is_exist("")
   end
 
   def is_exist(suffix) do
   end
 
   def exmlia(id, suffix) do
+    receive
+    {put, pid, key, value, ttl} ->
+      spawn_link(fn -> store(pid, key, value, ttl, suffix) end)
+      exmlia(id, suffix)
+    {get, pid, key} ->
+      spawn_link(fn -> find_value(pid, key, suffix) end)
+      exmlia(id, suffix)
+    {join, pid, host, port} ->
+      spawn_link(fn -> join(pid, id, host, port, suffix) end)
+      exmlia(id, suffix)
+    {port, pid} ->
+      to_atom(["udphandler"] ++ [suffix]) <- {port, pid}
+      exmlia(id, suffix)
+    true ->
+      exit(terminated)
+    end
   end
 
   def find_value(pid, key, suffix) do
