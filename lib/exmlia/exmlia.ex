6658ac3fb66ed160da027ab02e41610c59e510e8
Keisuke TAKAHASHI <keithseahus@gmail.com>
2014-08-01 23:54:09 +0900

    Update exmlia.ex

--- lib/exmlia/exmlia.ex
+++ lib/exmlia/exmlia.ex
@@ -41,42 +41,62 @@ defmodule Exmlia.exmlia do
   end
 
   def find_value(pid, key, suffix) do
+    to_atom(["rtoperator"] ++ suffix) <- {find_value, self, key}
+    receive
+    {find_value, key, value} -> pid <- {get, key, value}
+    end
   end
 
   def join(pid, my_id, host, post, suffix) do
+    to_atom(["rtoperator"] ++ suffix) <- {join, self, my_id, host, port}
+    receive
+    {join, true} -> pid <- {join, true}
+    {join, false} -> pid <- {join, false}
   end
 
   def store(pid, key, value, ttl, suffix) do
+    to_atom(["rtoperator"] ++ suffix) <- {store, self, key, value, ttl}
+    receive
+    {store, key, value, ttl} -> pid <- {put, key, value, ttl}
+    end
   end
 
   def put(pid, key, value, ttl) do
   end
 
   def put(pid, key, value, ttl, suffix) do
+    to_atom(["exmlia"] ++ suffix) <- {put, pid, key, value, ttl}
   end
 
   def get(pid, key) do
   end
 
   def get(pid, key, suffix) do
+    to_atom(["exmlia"] ++ suffix) <- {get, pid, key}
   end
 
   def join(pid, host, port) do
+    join(pid, host, port, "")
   end
 
   def join(pid, host, port, suffix) do
+    to_atom(["exmlia"] ++ suffix) <- {join, pid, host, port}
   end
 
   def get_port(pid) do
+    get_port(pid, "")
   end
 
   def get_port(pid, suffix) do
+    to_atom(["exmlia"] ++ suffix) <- {port, pid}
   end
 
   def stop() do
+    stop("")
   end
 
   def stop(suffix) do
+    to_atom(["exmlia"] ++ suffix) <- terminate
   end
 
 end
