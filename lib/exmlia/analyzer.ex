d30906c39dab8858b8f1c2ad11f5a6984a4b2ab1
Keisuke TAKAHASHI <keithseahus@gmail.com>
2014-08-03 23:39:42 +0900

    Update analyzer.ex

--- lib/exmlia/analyzer.ex
+++ lib/exmlia/analyzer.ex
@@ -10,12 +10,37 @@ defmodule analyzer do
   end
 
   def start0(n, m, host, port, is_churn) do
+    when n > 0
+    spawn(fn() -> start_node(host, port, (n - 1) * m, m, is_churn) end)
+    start0(n - 1, m, host, port, is_churn)
   end
 
   def start0(_, _, _, _, _) do
+    true
   end
 
   def start_node(host, port, offset, n, is_churn) do
+    suffix = to_list(offset)
+    Exmlia.exmlia.start(0, suffix)
+    receive
+    after 1000 do
+      true
+    end
+
+    join(host, port, 20, suffix)
+    put_data(offset, n, suffix)
+
+    cond do
+      is_churn == true -> Exmlia.exmlia.stop(suffix)
+      true -> true
+    end
+
+    receive
+    after 60000 do
+      true
+    end
+
+    try_get(offset, n, suffix, ?trynum, host, port, is_churn)
   end
 
   def try_get(offset, n, suffix, try_num, host, port, is_churn) do
